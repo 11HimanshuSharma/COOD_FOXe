cedfa010bc0f23544db285210b81a5bb21910de9
1Himanshusharma <himanshusharma272002@gmail.com>
2025-01-08 11:07:19 +0530

    .

--- Code_forces/800/Target_Practice.cpp
+++ Code_forces/800/Target_Practice.cpp
@@ -1,22 +1,31 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include<algorithm>
 using namespace std;
 
 int Solve(vector<vector<char>> &val) {
   int score = 0;
 
-  for (int i = 0; i < 10; i++) {
-    for (int j = 0; j < 10; j++) {
+  for (int i = 1; i <= 10; i++) {
+    for (int j = 1; j <= 10; j++) {
       if (val[i][j] == 'X') {
-        int case1 = abs(4 - i);
-        int case2 = abs(4 - j);
-        int case3 = abs(5 - i);
-        int case4 = abs(5 - j);
+        int row = i;
+        int col = j;
+        if (row > 5){
+          row = 10 - row + 1;
 
-        int point = 5 - min(case1,min(case2,min(case3,case4))) + 1;
-        score += point;
-      }
+        }
+        if (col > 5){
+          col = 10 - col + 1;
+        }
+        if (row == col ){
+          score += row;
+        }
+        else{
+          score += min(row,col);
+        }
+    }
     }
   }
   return score;
@@ -27,9 +36,9 @@ int main() {
   cin >> test_case;
 
   while (test_case--) {
-    vector<vector<char>> val(10, vector<char>(10));
-    for (int i = 0; i < 10; i++) {
-      for (int j = 0; j < 10; j++) {
+    vector<vector<char>> val(11, vector<char>(11));
+    for (int i = 1; i <= 10; i++) {
+      for (int j = 1; j <= 10; j++) {
         cin >> val[i][j];
       }
     }
